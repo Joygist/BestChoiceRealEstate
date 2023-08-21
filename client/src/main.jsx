512ded1f1f040c9d0ea8c72c6c18ebb908e7aea7
joygist <joygist.com@gmail.com>
2023-08-21 20:26:24 +0100

    RealEstate

---
+++ client/src/main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react"
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain="dev-l7g0mkynzfs7tmet.us.auth0.com"
+      clientId="tz1U9d1x7U3AEMw8UPSVMtEH4u9eShBX6prG"
+      authorizationParams={{
+        redirect_uri: "http://localhost:5173"
+      }}
+      audience="http://localhost:8000"
+      scope="openid profile"
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
